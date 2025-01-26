02a2f20e2de60fb942d851ca2b287ea5cc7d7faf
Wonhee <Qtsky89@gmail.com>
2025-01-26 08:45:15 +0000

    click color change

--- app.js
+++ app.js
@@ -1,40 +1,12 @@
-const title = document.querySelector("div.hello:first-child h1")
+const h1 = document.querySelector("div.hello:first-child h1")
 
-title.addEventListener("click", handleTitleClick)
-title.addEventListener("mouseenter", handleMouseEnter)
-title.addEventListener("mouseleave", handleMouseLeave)
+h1.addEventListener("click", handleTitleClick)
 
 function handleTitleClick() {
-    title.style.color = 'red'
-    console.log("title was clicked!")
-}
-
-function handleMouseEnter() {
-    title.innerText = "mouse is here"
-}
-
-function handleMouseLeave() {
-    title.innerText = "mouse is leave"
-}
-
-// window event
-window.addEventListener("resize", handleWindowResize)
-window.addEventListener("copy", handleWindowCopy)
-window.addEventListener("offline", handleWindowOffline)
-window.addEventListener("online", handleWindowOnline)
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato"
-}
-
-function handleWindowCopy() {
-    alert("copier")
-}
-
-function handleWindowOffline() {
-    alert("SOS no wifi")
-}
-
-function handleWindowOnline() {
-    alert("online!")
+    if (h1.style.color === "blue") {
+        h1.style.color = 'tomato'
+    } else {
+        h1.style.color = 'blue'
+    }
+       
 }
