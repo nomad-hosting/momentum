f2996bfbc2cd8bc11a93a4d2de5788e7717f5691
Wonhee <Qtsky89@gmail.com>
2025-01-26 07:23:18 +0000

    Changed color

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const title = document.querySelector("div h1:first-child")
+const title = document.querySelector("#title:first-child h1")
 
+title.innerText = "Got me"
+title.style.color = "blue";
 console.log(title.innerText)
