5ce398c1ea75056062cdfefac5603e38b9c09f38
Wonhee <Qtsky89@gmail.com>
2025-01-26 09:22:34 +0000

    Refactored

--- app.js
+++ app.js
@@ -3,7 +3,5 @@ const h1 = document.querySelector("div.hello:first-child h1")
 h1.addEventListener("click", handleTitleClick)
 
 function handleTitleClick() {
-    const clickedClass = "clicked"
-
-    h1.classList.toggle(clickedClass)
+    h1.classList.toggle("clicked")
 }
