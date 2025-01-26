402ee049ae96374e830682a4699dac7c2fc10c3b
Wonhee <Qtsky89@gmail.com>
2025-01-26 13:32:45 +0000

    clock done

--- js/clock.js
+++ js/clock.js
@@ -2,9 +2,11 @@ const clock = document.querySelector("h2#clock")
 
 function getClock() {
     const date = new Date()
-    const dateString = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    const hours = `${date.getHours()}`.padStart(2, "0")
+    const minutes = `${date.getMinutes()}`.padStart(2, "0")
+    const seconds = `${date.getSeconds()}`.padStart(2, "0")
 
-    clock.innerText = dateString
+    clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 
 getClock()
