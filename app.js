ac2082bc8453bc199b4404f01b57ec1b70a76b67
Wonhee <Qtsky89@gmail.com>
2025-01-26 08:21:39 +0000

    Added mouse enter event handler

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const title = document.querySelector("#title:first-child h1")
 
 title.addEventListener("click", handleTitleClick)
-
+title.addEventListener("mouseenter", handleMouseEnter)
 
 function handleTitleClick() {
     title.style.color = 'red'
     console.log("title was clicked!")
+}
+
+function handleMouseEnter() {
+    console.log("mouse is here")
 }
