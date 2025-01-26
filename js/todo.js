a781c388fdaa6b7c30511288db37732771bc65d0
Wonhee <Qtsky89@gmail.com>
2025-01-26 14:46:20 +0000

    Added delete button

--- js/todo.js
+++ js/todo.js
@@ -3,13 +3,26 @@ const todoInput = document.querySelector("#todo-form input")
 const todoList = document.querySelector("#todo-list")
 
 
+function deleteTodo(event) {
+    // this one is ok too
+    //todoList.removeChild(event.target.parentNode)
+
+    event.target.parentNode.remove()
+}
+
 function paintTodo(newTodo) {
     const newList = document.createElement("li")
+
     const span = document.createElement("span")
     newList.appendChild(span)
+    const button = document.createElement("button")  
+    button.innerText = "X"
+    button.classList.add("red")
+    button.addEventListener("click", deleteTodo)
+
+    newList.appendChild(button)
 
     span.innerText = newTodo
-
     todoList.appendChild(newList)
 }
 
