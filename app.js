860a6116cdb805b09a0f1daeb93dbc1a3fa26809
Wonhee <Qtsky89@gmail.com>
2025-01-26 06:20:40 +0000

    used getElementsByClassName

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const title = document.getElementById("title")
+const hellos = document.getElementsByClassName("hello")
 
-title.innerText = "Got you!"
-
-console.log(title.id)
-console.log(title.className)
+console.log(hellos)
