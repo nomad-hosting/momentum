a038cba994ca175bcceefb8494af1fc14a14abf6
Wonhee <Qtsky89@gmail.com>
2025-01-26 08:47:49 +0000

    Improved readability

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const h1 = document.querySelector("div.hello:first-child h1")
 h1.addEventListener("click", handleTitleClick)
 
 function handleTitleClick() {
-    if (h1.style.color === "blue") {
-        h1.style.color = 'tomato'
+    const currentColor = h1.style.color
+
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "tomato"
     } else {
-        h1.style.color = 'blue'
+        newColor = "blue"
     }
-       
+
+    h1.style.color = newColor
 }
