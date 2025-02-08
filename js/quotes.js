9499f6584741e937fef2dcad0d94fa1f63576171
Wonhee <Qtsky89@gmail.com>
2025-02-08 15:34:40 +0000

    css job

--- js/quotes.js
+++ js/quotes.js
@@ -1,23 +1,27 @@
 const quotes = [
     {
-        quote: "quote1",
-        author: "author1"
+        quote: "To live is the rarest thing in the world. Most people exist, that is all.",
+        author: "Oscar Wilde"
     },
     {
-        quote: "quote2",
-        author: "author2"
+        quote: "That it will never come again is what makes life so sweet.",
+        author: "Emily Dickinson"
     },
     {
-        quote: "quote3",
-        author: "author3"
+        quote: "It is never too late to be what you might have been.",
+        author: "George Eliot"
     },
     {
-        quote: "quote4",
-        author: "author4"
+        quote: "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+        author: "Ralph Waldo Emerson"
     },
     {
-        quote: "quote5",
-        author: "author5"
+        quote: "Pain is inevitable. Suffering is optional.",
+        author: "Haruki Murakami"
+    },
+    {
+        quote: "All the world's a stage, and all the men and women merely players.",
+        author: "William Shakespeare"
     },
 ]
 
