e27823486aaf155b0a16112c963e8b1a827b6206
Wonhee <Qtsky89@gmail.com>
2025-02-08 16:03:37 +0000

    Modified img path to relative

--- js/backgrounds.js
+++ js/backgrounds.js
@@ -5,4 +5,4 @@ const chosenImage = images[Math.floor(Math.random() * images.length)]
 // const bgImage = document.createElement("img")
 // bgImage.src = `/img/${chosenImage}`
 
-document.body.style.backgroundImage = `url('/img/${chosenImage}')`
+document.body.style.backgroundImage = `url('img/${chosenImage}')`
